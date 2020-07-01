aa31c223e2a595b636efca13d1ee92097373bd1c
TEldridge <skippydrumskins@yahoo.com>
2020-07-01 14:45:31 -0500

    saved JS

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
-const allowDrop = (e) => {
-  e.preventDefault();
+const allowDrop = (event) => {
+  event.preventDefault();
   // console.log(e.target.id)
-  document.getElementById(e.target.id).classList.add("hovering")
+  document.getElementById(event.target.id).classList.add("hovering")
+  document.getElementById(event.target.id).classList.remove("no-hover")
 }
+
+
 
 // TODO 2
 // Use the `allowDrop` function as a template to make this function 
 // be called when a draggable object exits a valid dropzone 
 // so the green background is removed.
-const stopDrop = (e) => {
+const removeDrop = (event) => {
+  event.preventDefault(); 
+  document.getElementById(event.target.id).classList.remove("hovering")
+  document.getElementById(event.target.id).classList.add("no-hover")
+  
   // write code here that will make the background return to normal when the dragged objects leaves a dropzone.
 }
 
-function drag(e) {
-  e.dataTransfer.setData("text", e.target.id);
+function drag(event) {
+  event.dataTransfer.setData("text", event.target.id);
 }
 
-const drop = (e) => {
-  e.preventDefault();
-  const data = e.dataTransfer.getData("text");
-  e.target.appendChild(document.getElementById(data));
+const drop = (event) => {
+  event.preventDefault();
+  const data = event.dataTransfer.getData("text");
+  event.target.appendChild(document.getElementById(data));
 }
